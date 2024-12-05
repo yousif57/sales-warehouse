dff475a8256921aed4fd12f381bf5254ece94254
yousif57 <yousifk8780@gmail.com>
2024-12-05 13:15:12 +0200

    v0.2

--- ETL_process.py
+++ ETL_process.py
@@ -489,8 +489,8 @@ features_mapping = {
     "BsmtHalfBath": "Basement_Half_Bathrooms",
     "FullBath": "Full_Bathrooms_Above_Grade",
     "HalfBath": "Half_Bathrooms_Above_Grade",
-    "Bedroom": "Bedrooms_Above_Grade",
-    "Kitchen": "Kitchens_Above_Grade",
+    "BedroomAbvGr": "Bedrooms_Above_Grade",
+    "KitchenAbvGr": "Kitchens_Above_Grade",
     "KitchenQual": "Kitchen_Quality",
     "TotRmsAbvGrd": "Total_Rooms_Above_Grade",
     "Functional": "Home_Functionality",
@@ -531,21 +531,278 @@ def Extract_transform():
     # remap elements to orginal form & fill in any missing data
     df_final = df.replace(elements_mappings).bfill()
     # rename columns to more readable form
-    df_final = df_final.rename(features_mapping, axis=1)
+    df_final = df_final.rename(columns = features_mapping)
 
     return df_final
 
-
-def load(connection_string, df):
+# load data to postgres database in DWH schema 
+def load_data(connection_string, df):
     engine = create_engine(connection_string)
 
     df.to_sql(name="main", con=engine, index=False, if_exists="replace")
 
-    engine.
+
+    with engine.connect() as connection:
+        with connection.begin():
+            # sales dim
+            connection.execute(text(
+                """
+                CREATE TABLE Sales_dim (
+                    "SalesId" SERIAL PRIMARY KEY,
+                    "Type_of_Sale" VARCHAR(255),
+                    "Sale_Condition" VARCHAR(255)
+                );
+                """))
+
+            connection.execute(text(
+                """
+                INSERT INTO Sales_dim ("Type_of_Sale", "Sale_Condition")
+                SELECT "Type_of_Sale", "Sale_Condition" FROM main;
+                """))
+
+            # date dim
+            connection.execute(text(
+                """
+                CREATE TABLE Date_dim (
+                    "DateId" SERIAL PRIMARY KEY,
+                    "Year_Built" INT,
+                    "Year_Remodeled" INT,
+                    "Garage_Year_Built" INT,
+                    "Month_Sold" INT,
+                    "Year_Sold" INT
+                );
+                """))
+            
+            connection.execute(text(
+                """
+                INSERT INTO Date_dim ("Year_Built", "Year_Remodeled", "Garage_Year_Built", "Month_Sold", "Year_Sold")
+                SELECT "Year_Built", "Year_Remodeled", "Garage_Year_Built", "Month_Sold", "Year_Sold"
+                FROM main;
+                """))
+
+            #location dim
+            connection.execute(text(
+                """
+                CREATE TABLE Location_dim (
+                    "LocationId" SERIAL PRIMARY KEY,
+                    "Street_Connection_Length_ft" INT,
+                    "Road_Access" VARCHAR(255),
+                    "Alley_Access" VARCHAR(255),
+                    "Property_Flatness" VARCHAR(255),
+                    "Property_Slope" VARCHAR(255),
+                    "Neighborhood_Location" VARCHAR(255),
+                    "Proximity_to_Condition1" VARCHAR(255),
+                    "Proximity_to_Condition2" VARCHAR(255),
+                    "Zoning_Classification" VARCHAR(255)
+                    );
+                """))
+
+            connection.execute(text(
+                """
+                INSERT INTO Location_dim (
+                    "Street_Connection_Length_ft", "Road_Access", "Alley_Access", "Property_Flatness", 
+                    "Property_Slope", "Neighborhood_Location", "Proximity_to_Condition1", 
+                    "Proximity_to_Condition2", "Zoning_Classification"
+                )
+                SELECT 
+                    "Street_Connection_Length_ft", "Road_Access", "Alley_Access", "Property_Flatness", 
+                    "Property_Slope", "Neighborhood_Location", "Proximity_to_Condition1", 
+                    "Proximity_to_Condition2", "Zoning_Classification"
+                FROM main;
+                """))
+
+            #quality dim
+            connection.execute(text(
+                """
+                CREATE TABLE Quality_dim (
+                    "QualityId" SERIAL PRIMARY KEY,
+                    "Overall_Material_Quality" VARCHAR(255),
+                    "Overall_Condition" VARCHAR(255),
+                    "Exterior_Quality" VARCHAR(255),
+                    "Exterior_Condition" VARCHAR(255),
+                    "Basement_Height_Quality" VARCHAR(255),
+                    "Basement_Condition" VARCHAR(255),
+                    "Basement_Exposure" VARCHAR(255),
+                    "Basement_Finish_Type1" VARCHAR(255),
+                    "Basement_Finish_Type2" VARCHAR(255),
+                    "Heating_Quality" VARCHAR(255),
+                    "Kitchen_Quality" VARCHAR(255),
+                    "Fireplace_Quality" VARCHAR(255),
+                    "Garage_Quality" VARCHAR(255),
+                    "Garage_Condition" VARCHAR(255),
+                    "Pool_Quality" VARCHAR(255),
+                    "Fence_Quality" VARCHAR(255)
+                );
+                """))
+
+            connection.execute(text(
+                """
+                INSERT INTO Quality_dim (
+                        "Overall_Material_Quality", "Overall_Condition", "Exterior_Quality", "Exterior_Condition", 
+                        "Basement_Height_Quality", "Basement_Condition", "Basement_Exposure", 
+                        "Basement_Finish_Type1", "Basement_Finish_Type2", "Heating_Quality", "Kitchen_Quality", 
+                        "Fireplace_Quality", "Garage_Quality", "Garage_Condition", "Pool_Quality", "Fence_Quality"
+                    )
+                    SELECT 
+                        "Overall_Material_Quality", "Overall_Condition", "Exterior_Quality", "Exterior_Condition", 
+                        "Basement_Height_Quality", "Basement_Condition", "Basement_Exposure", 
+                        "Basement_Finish_Type1", "Basement_Finish_Type2", "Heating_Quality", "Kitchen_Quality", 
+                        "Fireplace_Quality", "Garage_Quality", "Garage_Condition", "Pool_Quality", "Fence_Quality"
+                    FROM main;
+                """))
+
+            # Construction_dim
+            connection.execute(text(
+                """
+                CREATE TABLE Construction_dim (
+                    "ConstructionId" SERIAL PRIMARY KEY,
+                    "Building_Class" VARCHAR(255),
+                    "Property_Shape" VARCHAR(255),
+                    "Lot_Configuration" VARCHAR(255),
+                    "Building_Type" VARCHAR(255),
+                    "House_Style" VARCHAR(255),
+                    "Roof_Style" VARCHAR(255),
+                    "Roof_Material" VARCHAR(255),
+                    "Exterior_Covering1" VARCHAR(255),
+                    "Exterior_Covering2" VARCHAR(255),
+                    "Masonry_Veneer_Type" VARCHAR(255),
+                    "Masonry_Veneer_Area_sq_ft" INT,
+                    "Foundation_Type" VARCHAR(255),
+                    "Home_Functionality" VARCHAR(255)
+                );
+                """))
+
+            connection.execute(text(
+                """
+                INSERT INTO Construction_dim (
+                    "Building_Class", "Property_Shape", "Lot_Configuration", "Building_Type", "House_Style", 
+                    "Roof_Style", "Roof_Material", "Exterior_Covering1", "Exterior_Covering2", 
+                    "Masonry_Veneer_Type", "Masonry_Veneer_Area_sq_ft", "Foundation_Type", "Home_Functionality"
+                )
+                SELECT 
+                    "Building_Class", "Property_Shape", "Lot_Configuration", "Building_Type", "House_Style", 
+                    "Roof_Style", "Roof_Material", "Exterior_Covering1", "Exterior_Covering2", 
+                    "Masonry_Veneer_Type", "Masonry_Veneer_Area_sq_ft", "Foundation_Type", "Home_Functionality"
+                FROM main;
+                """))
+
+            # Facilities_dim
+            connection.execute(text(
+                """
+                CREATE TABLE Facilities_dim (
+                    "FacilitiesId" SERIAL PRIMARY KEY,
+                    "Heating_Type" VARCHAR(255),
+                    "Electrical_System" VARCHAR(255),
+                    "Central_Air_Conditioning" VARCHAR(255),
+                    "Number_of_Fireplaces" INT,
+                    "Pool_Area_sq_ft" INT,
+                    "Garage_Area_sq_ft" INT,
+                    "Garage_Capacity_Cars" INT,
+                    "Garage_Interior_Finish" VARCHAR(255),
+                    "Garage_Location_Type" VARCHAR(255),
+                    "Driveway_Surface_Type" VARCHAR(255),
+                    "Screen_Porch_Area_sq_ft" INT,
+                    "Three_Season_Porch_Area_sq_ft" INT,
+                    "Enclosed_Porch_Area_sq_ft" INT,
+                    "Open_Porch_Area_sq_ft" INT,
+                    "Wood_Deck_Area_sq_ft" INT,
+                    "Utilities_Available" VARCHAR(255),
+                    "Miscellaneous_Feature" VARCHAR(255),
+                    "Miscellaneous_Value_usd" NUMERIC
+                );
+                """))
+
+            connection.execute(text(
+                """
+                INSERT INTO Facilities_dim (
+                    "Heating_Type", "Electrical_System", "Central_Air_Conditioning", "Number_of_Fireplaces", 
+                    "Pool_Area_sq_ft", "Garage_Area_sq_ft", "Garage_Capacity_Cars", "Garage_Interior_Finish", 
+                    "Garage_Location_Type", "Driveway_Surface_Type", "Screen_Porch_Area_sq_ft", 
+                    "Three_Season_Porch_Area_sq_ft", "Enclosed_Porch_Area_sq_ft", "Open_Porch_Area_sq_ft", 
+                    "Wood_Deck_Area_sq_ft", "Utilities_Available", "Miscellaneous_Feature", "Miscellaneous_Value_usd"
+                )
+                SELECT 
+                    "Heating_Type", "Electrical_System", "Central_Air_Conditioning", "Number_of_Fireplaces", 
+                    "Pool_Area_sq_ft", "Garage_Area_sq_ft", "Garage_Capacity_Cars", "Garage_Interior_Finish", 
+                    "Garage_Location_Type", "Driveway_Surface_Type", "Screen_Porch_Area_sq_ft", 
+                    "Three_Season_Porch_Area_sq_ft", "Enclosed_Porch_Area_sq_ft", "Open_Porch_Area_sq_ft", 
+                    "Wood_Deck_Area_sq_ft", "Utilities_Available", "Miscellaneous_Feature", "Miscellaneous_Value_usd"
+                FROM main;
+                """))
+
+            # property_fact
+            connection.execute(text(
+                """
+                CREATE TABLE Property_fact (
+                    "PropertyFactId" SERIAL PRIMARY KEY,
+                    "SalesId" INT,
+                    "DateId" INT,
+                    "LocationId" INT,
+                    "QualityId" INT,
+                    "ConstructionId" INT,
+                    "FacilitiesId" INT,
+                    "Lot_Size_sq_ft" INT,
+                    "Total_Basement_Area_sq_ft" INT,
+                    "Unfinished_Basement_Area_sq_ft" INT,
+                    "Basement_Finished_Area1_sq_ft" INT,
+                    "Basement_Finished_Area2_sq_ft" INT,
+                    "Above_Grade_Living_Area_sq_ft" INT,
+                    "Low_Quality_Finished_Area_sq_ft" INT,
+                    "First_Floor_Area_sq_ft" INT,
+                    "Half_Bathrooms_Above_Grade" INT,
+                    "Full_Bathrooms_Above_Grade" INT,
+                    "Basement_Half_Bathrooms" INT,
+                    "Basement_Full_Bathrooms" INT,
+                    "Second_Floor_Area_sq_ft" INT,
+                    "Bedrooms_Above_Grade" INT,
+                    "Kitchens_Above_Grade" INT,
+                    "Total_Rooms_Above_Grade" INT,
+                    FOREIGN KEY ("SalesId") REFERENCES Sales_dim("SalesId"),
+                    FOREIGN KEY ("DateId") REFERENCES Date_dim("DateId"),
+                    FOREIGN KEY ("LocationId") REFERENCES Location_dim("LocationId"),
+                    FOREIGN KEY ("QualityId") REFERENCES Quality_dim("QualityId"),
+                    FOREIGN KEY ("ConstructionId") REFERENCES Construction_dim("ConstructionId"),
+                    FOREIGN KEY ("FacilitiesId") REFERENCES Facilities_dim("FacilitiesId")
+                );
+                """))
+
+            connection.execute(text(
+                """
+                INSERT INTO Property_fact (
+                    "Lot_Size_sq_ft", "Total_Basement_Area_sq_ft", "Unfinished_Basement_Area_sq_ft", 
+                    "Basement_Finished_Area1_sq_ft", "Basement_Finished_Area2_sq_ft", 
+                    "Above_Grade_Living_Area_sq_ft", "Low_Quality_Finished_Area_sq_ft", 
+                    "First_Floor_Area_sq_ft", "Half_Bathrooms_Above_Grade", "Full_Bathrooms_Above_Grade", 
+                    "Basement_Half_Bathrooms", "Basement_Full_Bathrooms", "Second_Floor_Area_sq_ft", 
+                    "Bedrooms_Above_Grade", "Kitchens_Above_Grade", "Total_Rooms_Above_Grade"
+                )
+                SELECT
+                    "Lot_Size_sq_ft", "Total_Basement_Area_sq_ft", "Unfinished_Basement_Area_sq_ft", 
+                    "Basement_Finished_Area1_sq_ft", "Basement_Finished_Area2_sq_ft", 
+                    "Above_Grade_Living_Area_sq_ft", "Low_Quality_Finished_Area_sq_ft", 
+                    "First_Floor_Area_sq_ft", "Half_Bathrooms_Above_Grade", "Full_Bathrooms_Above_Grade", 
+                    "Basement_Half_Bathrooms", "Basement_Full_Bathrooms", "Second_Floor_Area_sq_ft", 
+                    "Bedrooms_Above_Grade", "Kitchens_Above_Grade", "Total_Rooms_Above_Grade"
+                FROM main;
+                """))
+    
+            #drop main table
+            connection.execute(text(
+                """
+                DROP TABLE main;
+                """))
+
+
+            connection.commit()
+
 
 
 
 if __name__ == "__main__":
     connection_string = "postgresql://postgres:root@localhost:5432/postgres"
 
+    data = Extract_transform()
+
+
+    load_data(connection_string, data)
     
--- database/schema.sql
+++ database/schema.sql
@@ -1,87 +1,208 @@
 -- Active: 1731745085190@@127.0.0.1@5432@postgres
-SELECT * from main;
 
-# sales dim
-CREATE TABLE Sales_dim AS
-SELECT "Type_of_Sale", "Sale_Condition"
-FROM main;
+#Sales Dimension
+CREATE TABLE Sales_dim (
+    "SalesId" SERIAL PRIMARY KEY,
+    "Type_of_Sale" VARCHAR(255),
+    "Sale_Condition" VARCHAR(255)
+);
 
-ALTER TABLE Sales_dim
-ADD COLUMN "SalesId" SERIAL PRIMARY KEY;
+INSERT INTO Sales_dim ("Type_of_Sale", "Sale_Condition")
+SELECT "Type_of_Sale", "Sale_Condition" FROM main;
+#------------------------------------------------------------------
 
-SELECT * from sales_dim;
-#----------------------------------------------------------------
+#date dim;
+CREATE TABLE Date_dim (
+    "DateId" SERIAL PRIMARY KEY,
+    "Year_Built" INT,
+    "Year_Remodeled" INT,
+    "Garage_Year_Built" INT,
+    "Month_Sold" INT,
+    "Year_Sold" INT
+);
 
-# date dim
-CREATE TABLE Date_dim AS
+INSERT INTO Date_dim ("Year_Built", "Year_Remodeled", "Garage_Year_Built", "Month_Sold", "Year_Sold")
 SELECT "Year_Built", "Year_Remodeled", "Garage_Year_Built", "Month_Sold", "Year_Sold"
 FROM main;
-
-ALTER TABLE Date_dim
-ADD COLUMN "DateId" SERIAL PRIMARY KEY;
-#-----------------------------------------------------------------
+#------------------------------------------------------------------
 
 #location dim
-CREATE TABLE Location_dim AS
-select "Street_Connection_Length_ft","Road_Access","Alley_Access","Property_Flatness",
-    "Property_Slope","Neighborhood_Location","Proximity_to_Condition1","Proximity_to_Condition2",
-    "Zoning_Classification"
-from main;
+CREATE TABLE Location_dim (
+    "LocationId" SERIAL PRIMARY KEY,
+    "Street_Connection_Length_ft" INT,
+    "Road_Access" VARCHAR(255),
+    "Alley_Access" VARCHAR(255),
+    "Property_Flatness" VARCHAR(255),
+    "Property_Slope" VARCHAR(255),
+    "Neighborhood_Location" VARCHAR(255),
+    "Proximity_to_Condition1" VARCHAR(255),
+    "Proximity_to_Condition2" VARCHAR(255),
+    "Zoning_Classification" VARCHAR(255)
+);
 
-ALTER TABLE location_dim
-ADD COLUMN "LocationId" SERIAL PRIMARY KEY;
+INSERT INTO Location_dim (
+    "Street_Connection_Length_ft", "Road_Access", "Alley_Access", "Property_Flatness", 
+    "Property_Slope", "Neighborhood_Location", "Proximity_to_Condition1", 
+    "Proximity_to_Condition2", "Zoning_Classification"
+)
+SELECT 
+    "Street_Connection_Length_ft", "Road_Access", "Alley_Access", "Property_Flatness", 
+    "Property_Slope", "Neighborhood_Location", "Proximity_to_Condition1", 
+    "Proximity_to_Condition2", "Zoning_Classification"
+FROM main;
 #------------------------------------------------------------------
 
 #quality dim
-CREATE TABLE Quality_dim AS
-Select "Overall_Material_Quality", "Overall_Condition", "Exterior_Quality", "Exterior_Condition", "Basement_Height_Quality",
-    "Basement_Condition", "Basement_Exposure", "Basement_Finish_Type1", "Basement_Finish_Type2", "Heating_Quality", 
-    "Kitchen_Quality", "Fireplace_Quality", "Garage_Quality", "Garage_Condition", "Pool_Quality", "Fence_Quality"
-FROM main;
+CREATE TABLE Quality_dim (
+    "QualityId" SERIAL PRIMARY KEY,
+    "Overall_Material_Quality" VARCHAR(255),
+    "Overall_Condition" VARCHAR(255),
+    "Exterior_Quality" VARCHAR(255),
+    "Exterior_Condition" VARCHAR(255),
+    "Basement_Height_Quality" VARCHAR(255),
+    "Basement_Condition" VARCHAR(255),
+    "Basement_Exposure" VARCHAR(255),
+    "Basement_Finish_Type1" VARCHAR(255),
+    "Basement_Finish_Type2" VARCHAR(255),
+    "Heating_Quality" VARCHAR(255),
+    "Kitchen_Quality" VARCHAR(255),
+    "Fireplace_Quality" VARCHAR(255),
+    "Garage_Quality" VARCHAR(255),
+    "Garage_Condition" VARCHAR(255),
+    "Pool_Quality" VARCHAR(255),
+    "Fence_Quality" VARCHAR(255)
+);
 
-ALTER TABLE Quality_dim
-ADD COLUMN "QualityId" SERIAL PRIMARY KEY;
+INSERT INTO Quality_dim (
+    "Overall_Material_Quality", "Overall_Condition", "Exterior_Quality", "Exterior_Condition", 
+    "Basement_Height_Quality", "Basement_Condition", "Basement_Exposure", 
+    "Basement_Finish_Type1", "Basement_Finish_Type2", "Heating_Quality", "Kitchen_Quality", 
+    "Fireplace_Quality", "Garage_Quality", "Garage_Condition", "Pool_Quality", "Fence_Quality"
+)
+SELECT 
+    "Overall_Material_Quality", "Overall_Condition", "Exterior_Quality", "Exterior_Condition", 
+    "Basement_Height_Quality", "Basement_Condition", "Basement_Exposure", 
+    "Basement_Finish_Type1", "Basement_Finish_Type2", "Heating_Quality", "Kitchen_Quality", 
+    "Fireplace_Quality", "Garage_Quality", "Garage_Condition", "Pool_Quality", "Fence_Quality"
+FROM main;
 #----------------------------------------------------------------------
 
 # Construction_dim
-CREATE TABLE Construction_dim AS
-SELECT "Building_Class", "Property_Shape", "Lot_Configuration", "Building_Type", "House_Style", "Roof_Style", "Roof_Material"
-    "Exterior_Covering1", "Exterior_Covering2", "Masonry_Veneer_Type", "Masonry_Veneer_Area_sq_ft", "Foundation_Type", 
-    "Home_Functionality"
-FROM main;
+CREATE TABLE Construction_dim (
+    "ConstructionId" SERIAL PRIMARY KEY,
+    "Building_Class" VARCHAR(255),
+    "Property_Shape" VARCHAR(255),
+    "Lot_Configuration" VARCHAR(255),
+    "Building_Type" VARCHAR(255),
+    "House_Style" VARCHAR(255),
+    "Roof_Style" VARCHAR(255),
+    "Roof_Material" VARCHAR(255),
+    "Exterior_Covering1" VARCHAR(255),
+    "Exterior_Covering2" VARCHAR(255),
+    "Masonry_Veneer_Type" VARCHAR(255),
+    "Masonry_Veneer_Area_sq_ft" INT,
+    "Foundation_Type" VARCHAR(255),
+    "Home_Functionality" VARCHAR(255)
+);
 
-ALTER TABLE Construction_dim
-ADD COLUMN "ConstructionId" SERIAL PRIMARY KEY;
+INSERT INTO Construction_dim (
+    "Building_Class", "Property_Shape", "Lot_Configuration", "Building_Type", "House_Style", 
+    "Roof_Style", "Roof_Material", "Exterior_Covering1", "Exterior_Covering2", 
+    "Masonry_Veneer_Type", "Masonry_Veneer_Area_sq_ft", "Foundation_Type", "Home_Functionality"
+)
+SELECT 
+    "Building_Class", "Property_Shape", "Lot_Configuration", "Building_Type", "House_Style", 
+    "Roof_Style", "Roof_Material", "Exterior_Covering1", "Exterior_Covering2", 
+    "Masonry_Veneer_Type", "Masonry_Veneer_Area_sq_ft", "Foundation_Type", "Home_Functionality"
+FROM main;
 #---------------------------------------------------------------------
 
 # Facilities_dim
-CREATE TABLE Facilities_dim AS
-SELECT "Heating_Type", "Electrical_System", "Central_Air_Conditioning", "Number_of_Fireplaces", "Pool_Area_sq_ft", 
-    "Garage_Area_sq_ft", "Garage_Capacity_Cars", "Garage_Interior_Finish", "Garage_Location_Type", "Driveway_Surface_Type",
-    "Screen_Porch_Area_sq_ft", "Three_Season_Porch_Area_sq_ft", "Enclosed_Porch_Area_sq_ft", "Open_Porch_Area_sq_ft", 
-    "Wood_Deck_Area_sq_ft", "Utilities_Available", "Miscellaneous_Feature", "Miscellaneous_Value_usd"
-    
-FROM main;
+CREATE TABLE Facilities_dim (
+    "FacilitiesId" SERIAL PRIMARY KEY,
+    "Heating_Type" VARCHAR(255),
+    "Electrical_System" VARCHAR(255),
+    "Central_Air_Conditioning" VARCHAR(255),
+    "Number_of_Fireplaces" INT,
+    "Pool_Area_sq_ft" INT,
+    "Garage_Area_sq_ft" INT,
+    "Garage_Capacity_Cars" INT,
+    "Garage_Interior_Finish" VARCHAR(255),
+    "Garage_Location_Type" VARCHAR(255),
+    "Driveway_Surface_Type" VARCHAR(255),
+    "Screen_Porch_Area_sq_ft" INT,
+    "Three_Season_Porch_Area_sq_ft" INT,
+    "Enclosed_Porch_Area_sq_ft" INT,
+    "Open_Porch_Area_sq_ft" INT,
+    "Wood_Deck_Area_sq_ft" INT,
+    "Utilities_Available" VARCHAR(255),
+    "Miscellaneous_Feature" VARCHAR(255),
+    "Miscellaneous_Value_usd" NUMERIC
+);
 
-ALTER TABLE Facilities_dim
-ADD COLUMN "FacilitiesId" SERIAL PRIMARY KEY;
+INSERT INTO Facilities_dim (
+    "Heating_Type", "Electrical_System", "Central_Air_Conditioning", "Number_of_Fireplaces", 
+    "Pool_Area_sq_ft", "Garage_Area_sq_ft", "Garage_Capacity_Cars", "Garage_Interior_Finish", 
+    "Garage_Location_Type", "Driveway_Surface_Type", "Screen_Porch_Area_sq_ft", 
+    "Three_Season_Porch_Area_sq_ft", "Enclosed_Porch_Area_sq_ft", "Open_Porch_Area_sq_ft", 
+    "Wood_Deck_Area_sq_ft", "Utilities_Available", "Miscellaneous_Feature", "Miscellaneous_Value_usd"
+)
+SELECT 
+    "Heating_Type", "Electrical_System", "Central_Air_Conditioning", "Number_of_Fireplaces", 
+    "Pool_Area_sq_ft", "Garage_Area_sq_ft", "Garage_Capacity_Cars", "Garage_Interior_Finish", 
+    "Garage_Location_Type", "Driveway_Surface_Type", "Screen_Porch_Area_sq_ft", 
+    "Three_Season_Porch_Area_sq_ft", "Enclosed_Porch_Area_sq_ft", "Open_Porch_Area_sq_ft", 
+    "Wood_Deck_Area_sq_ft", "Utilities_Available", "Miscellaneous_Feature", "Miscellaneous_Value_usd"
+FROM main;
 #---------------------------------------------------------------------
 
 # property_fact
-CREATE TABLE Property_fact AS
-SELECT "Lot_Size_sq_ft", "Total_Basement_Area_sq_ft", "Unfinished_Basement_Area_sq_ft", "Basement_Finished_Area1_sq_ft",
-    "Basement_Finished_Area2_sq_ft", "Above_Grade_Living_Area_sq_ft", "Low_Quality_Finished_Area_sq_ft", "First_Floor_Area_sq_ft", 
-    "Half_Bathrooms_Above_Grade", "Full_Bathrooms_Above_Grade", "Basement_Half_Bathrooms", 
-    "Basement_Full_Bathrooms", "Second_Floor_Area_sq_ft", "Bedrooms_Above_Grade", "Kitchens_Above_Grade", 
-    "Total_Rooms_Above_Grade"
+CREATE TABLE Property_fact (
+    "PropertyFactId" SERIAL PRIMARY KEY,
+    "SalesId" INT,
+    "DateId" INT,
+    "LocationId" INT,
+    "QualityId" INT,
+    "ConstructionId" INT,
+    "FacilitiesId" INT,
+    "Lot_Size_sq_ft" INT,
+    "Total_Basement_Area_sq_ft" INT,
+    "Unfinished_Basement_Area_sq_ft" INT,
+    "Basement_Finished_Area1_sq_ft" INT,
+    "Basement_Finished_Area2_sq_ft" INT,
+    "Above_Grade_Living_Area_sq_ft" INT,
+    "Low_Quality_Finished_Area_sq_ft" INT,
+    "First_Floor_Area_sq_ft" INT,
+    "Half_Bathrooms_Above_Grade" INT,
+    "Full_Bathrooms_Above_Grade" INT,
+    "Basement_Half_Bathrooms" INT,
+    "Basement_Full_Bathrooms" INT,
+    "Second_Floor_Area_sq_ft" INT,
+    "Bedrooms_Above_Grade" INT,
+    "Kitchens_Above_Grade" INT,
+    "Total_Rooms_Above_Grade" INT,
+    FOREIGN KEY ("SalesId") REFERENCES Sales_dim("SalesId"),
+    FOREIGN KEY ("DateId") REFERENCES Date_dim("DateId"),
+    FOREIGN KEY ("LocationId") REFERENCES Location_dim("LocationId"),
+    FOREIGN KEY ("QualityId") REFERENCES Quality_dim("QualityId"),
+    FOREIGN KEY ("ConstructionId") REFERENCES Construction_dim("ConstructionId"),
+    FOREIGN KEY ("FacilitiesId") REFERENCES Facilities_dim("FacilitiesId")
+);
+
+INSERT INTO Property_fact (
+    "Lot_Size_sq_ft", "Total_Basement_Area_sq_ft", "Unfinished_Basement_Area_sq_ft", 
+    "Basement_Finished_Area1_sq_ft", "Basement_Finished_Area2_sq_ft", 
+    "Above_Grade_Living_Area_sq_ft", "Low_Quality_Finished_Area_sq_ft", 
+    "First_Floor_Area_sq_ft", "Half_Bathrooms_Above_Grade", "Full_Bathrooms_Above_Grade", 
+    "Basement_Half_Bathrooms", "Basement_Full_Bathrooms", "Second_Floor_Area_sq_ft", 
+    "Bedrooms_Above_Grade", "Kitchens_Above_Grade", "Total_Rooms_Above_Grade"
+)
+SELECT
+    "Lot_Size_sq_ft", "Total_Basement_Area_sq_ft", "Unfinished_Basement_Area_sq_ft", 
+    "Basement_Finished_Area1_sq_ft", "Basement_Finished_Area2_sq_ft", 
+    "Above_Grade_Living_Area_sq_ft", "Low_Quality_Finished_Area_sq_ft", 
+    "First_Floor_Area_sq_ft", "Half_Bathrooms_Above_Grade", "Full_Bathrooms_Above_Grade", 
+    "Basement_Half_Bathrooms", "Basement_Full_Bathrooms", "Second_Floor_Area_sq_ft", 
+    "Bedrooms_Above_Grade", "Kitchens_Above_Grade", "Total_Rooms_Above_Grade"
 FROM main;
-
-ALTER TABLE Property_fact
-ADD CONSTRAINT "SalesId" Foreign Key ("SalesId") REFERENCES Sales_dim("SalesId"),
-ADD CONSTRAINT "DateId" Foreign Key ("DateId") REFERENCES Date_dim("DateId"),
-ADD CONSTRAINT "LocationId" Foreign Key ("LocationId") REFERENCES Location_dim("LocationId"),
-ADD CONSTRAINT "QualityId" Foreign Key ("QualityId") REFERENCES Quality_dim("QualityId"),
-ADD CONSTRAINT "ConstructionId" Foreign Key ("ConstructionId") REFERENCES Construction_dim("ConstructionId"),
-ADD CONSTRAINT "FacilitiesId" Foreign Key ("FacilitiesId") REFERENCES Facilities_dim("FacilitiesId");
-#---------------------------------------------------------------------
-
+#------------------------------------------------------------------
